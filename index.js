5d0fe482ec33f93cc2b119de8fc80bd50c20b1a3
winstoncwang <congwang@live.co.uk>
2020-04-28 22:06:37 +0100

    V 0.142
    using promise appoach with fs.promise and util.promisify to allow all results to be return and displayed at once.

--- index.js
+++ index.js
@@ -1,36 +1,64 @@
 //fs module
 
 const fs = require('fs');
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const lstat = util.promisify(fs.lstat); //appropriate Method 1
+
+// const { lstat } = fs.promises;  //appropriate Method 2
+
+fs.readdir(process.cwd(), async (err, filenames) => {
 	//process is a global module made available already
 	if (err) {
-		console.log(err);
-		return;
+		throw new Error(err);
 	}
 
-	const allStats = Array(filenames.length).fill(null);
-
-	filenames.forEach((filename, index) => {
-		fs.lstat(filename, (err, stat) => {
-			if (err) {
-				throw new Error(err);
-			}
-			allStats[index] = stat;
-			// if (!allStats.includes(null)) {
-			// 	allStats.forEach((stats, index) => {
-			// 		console.log(filenames[index], stats);
-			// 	});
-			// }
-			const ready = allStats.every((stats) => {
-				return stats;
-			});
-
-			if (ready) {
-				allStats.forEach((stats, index) => {
-					console.log(filenames[index], stats.isFile());
-				});
-			}
-		});
-	});
+	for (let filename of filenames) {
+		try {
+			const stats = await lstat(filename);
+			console.log(filename, stats.isFile());
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 });
+
+// const lstat = () => {
+// 	return new Promise((resolve, reject) => {
+// 		fs.lstat(filename, (err, stats) => {
+// 			if (err) {
+// 				reject(err);                              //solution 3
+// 			}
+
+// 			resolve(stats);
+// 		});
+// 	});
+// };
+
+//const allStats = Array(filenames.length).fill(null);
+
+// filenames.forEach((filename, index) => {
+
+// 	fs.lstat(filename, (err, stat) => {
+// 		if (err) {
+// 			throw new Error(err);
+// 		}
+//         allStats[index] = stat;
+
+// 		if (!allStats.includes(null)) {
+// 			allStats.forEach((stats, index) => {
+// 				console.log(filenames[index], stats);    //solution 1
+// 			});
+//         }
+
+// 		const ready = allStats.every((stats) => {
+// 			return stats;
+// 		});
+//                                                        //solution 2
+// 		if (ready) {
+// 			allStats.forEach((stats, index) => {
+// 				console.log(filenames[index], stats.isFile());
+// 			});
+// 		}
+// 	});
+// });
