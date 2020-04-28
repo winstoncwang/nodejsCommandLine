83b7a1e61e9f5cff293059ee5de22ebe18225331
winstoncwang <congwang@live.co.uk>
2020-04-28 22:54:05 +0100

    V 0.18
    fixed bug with the filename when using fs.lstat.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const util = require('util');
 const chalk = require('chalk');
+const path = require('path');
 
 const targetDir = process.argv[2] || process.cwd(); //input from node with argument[2] (argv) or the current working directory
 const lstat = util.promisify(fs.lstat); //appropriate Method 1
@@ -16,7 +17,7 @@ fs.readdir(targetDir, async (err, filenames) => {
 	}
 
 	const statPromises = filenames.map((filename) => {
-		return lstat(filename);
+		return lstat(path.join(targetDir, filename)); //joins to form a full path rather than just the filename
 	});
 
 	const allStats = await Promise.all(statPromises); //this takes full advantage of async/promise as this only returns when all promises are resolved.
